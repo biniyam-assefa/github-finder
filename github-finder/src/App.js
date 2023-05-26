8cebdf54522ab267baadda02d40667da393f860a
biniyam-assefa <biniyoung5@gmail.com>
2023-05-26 16:44:32 +0300

    .env

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,30 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbarr from "./components/layout/Navbarr";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./Context/github/GithubContext";
+import { AlertProvider } from "./Context/Alert/Alert.Context";
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbarr />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-              <Route></Route>
-              <Route></Route>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbarr />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+                <Route></Route>
+                <Route></Route>
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
--- github-finder/src/Context/github/GithubContext.js
+++ github-finder/src/Context/github/GithubContext.js
@@ -4,30 +4,81 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    user: {},
+    repos: [],
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`);
-    const data = await response.json();
+  //GET search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    const { items } = await response.json();
 
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
+
+  //Get user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.staus === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
+  // Clear users from state
+  const clearUsers = () => {
+    dispatch({
+      type: "CLEAR_USERS",
+    });
+  };
+
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        user: state.user,
+        repos: state.repos,
+        searchUsers,
+        clearUsers,
+        getUser,
       }}
     >
       {children}
